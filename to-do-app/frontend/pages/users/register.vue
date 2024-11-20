ce5ade7419c749c9885b8ad747a6e9da37966ce3
COC.ThienND <thiennd@concrete-corp.com>
2024-11-20 10:48:41 +0700

    remove public

--- to-do-app/frontend/pages/users/register.vue
+++ to-do-app/frontend/pages/users/register.vue
@@ -37,24 +37,21 @@
                                 </div>
                                 <form>
                                     <div class="mb-3">
-                                        <label for="exampleInputEmail1" class="form-label">Username</label>
+                                        <label for="exampleInputUsername" class="form-label">Username</label>
+                                        <input type="email" class="form-control" id="exampleInputUsername" aria-describedby="userNameHelp">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="exampleInputEmail1" class="form-label">Email</label>
                                         <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                                     </div>
                                     <div class="mb-4">
                                         <label for="exampleInputPassword1" class="form-label">Password</label>
                                         <input type="password" class="form-control" id="exampleInputPassword1">
                                     </div>
-                                    <div class="d-flex align-items-center justify-content-between mb-4">
-                                        <div class="form-check">
-                                            <input class="form-check-input primary" type="checkbox" value="" id="flexCheckChecked" checked="">
-                                            <label class="form-check-label text-dark fs-3" for="flexCheckChecked"> Remeber this Device </label>
-                                        </div>
-                                        <a class="text-primary fw-medium fs-3" href="#">Forgot Password ?</a>
-                                    </div>
-                                    <a href="#" class="btn btn-primary w-100 py-8 mb-4 rounded-2">Sign In</a>
+                                    <a href="#" class="btn btn-primary w-100 py-8 mb-4 rounded-2">Register account</a>
                                     <div class="d-flex align-items-center justify-content-center">
-                                        <p class="fs-4 mb-0 fw-medium">New to Modernize?</p>
-                                        <a class="text-primary fw-medium ms-2" href="#">Create an account</a>
+                                        <p class="fs-4 mb-0 fw-medium">You have account?</p>
+                                        <a class="text-primary fw-medium ms-2" href="#">Sign In</a>
                                     </div>
                                 </form>
                             </div>
